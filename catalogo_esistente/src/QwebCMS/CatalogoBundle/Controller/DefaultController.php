e844d9cdcbe8a480daa16c112bfe5f1d800b98c8
Mattia Clementi <mattia@qweb.eu>
2015-05-20 18:30:26 +0200

    Aggiunto Controller "Security" che gestisce il login; Aggiunto template di login; Aggiunte direttive nel file security.yml;

--- catalogo_esistente/src/QwebCMS/CatalogoBundle/Controller/DefaultController.php
+++ catalogo_esistente/src/QwebCMS/CatalogoBundle/Controller/DefaultController.php
@@ -3,11 +3,17 @@
 namespace QwebCMS\CatalogoBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     public function indexAction($name)
     {
         return $this->render('QwebCMSCatalogoBundle:Default:index.html.twig', array('name' => $name));
+    }
+    
+    public function adminAction()
+    {
+        return new Response('Pagina admin!');
     }
 }
