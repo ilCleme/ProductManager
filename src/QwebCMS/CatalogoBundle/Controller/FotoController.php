cbf68c0fdb268ac83e4e9c61e5065fcb89ca0da0
Mattia Clementi <mattia@qweb.eu>
2015-09-02 16:53:41 +0200

    Inserita gestione di posizione per le foto prodotto

--- src/QwebCMS/CatalogoBundle/Controller/FotoController.php
+++ src/QwebCMS/CatalogoBundle/Controller/FotoController.php
@@ -3,6 +3,8 @@
 namespace QwebCMS\CatalogoBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use QwebCMS\CatalogoBundle\Entity\TblPhoto as Foto;
 
 class FotoController extends Controller
 {
@@ -24,5 +26,49 @@ class FotoController extends Controller
         return $this->render(
             'QwebCMSCatalogoBundle:Photo:delete.html.twig'
         );
+    }
+
+    public function updatePositionAction(){
+
+        $request = $this->container->get('request');
+        $data = $request->query->get('foto');
+        $data = json_decode($data, true);
+
+        $em = $this->getDoctrine()->getManager();
+
+        $response = array("code" => 100, "success" => true);
+
+        foreach($data as $key=>$value){
+
+            if($response['success']){
+                $foto = $this->getDoctrine()
+                    ->getRepository('QwebCMSCatalogoBundle:TblPhoto')
+                    ->find($value['id']);
+
+                if (!$foto) {
+                    /*$this->get('session')->getFlashBag()->add(
+                        'danger',
+                        'É avvenuto un errore. Non è stato possibile ordinare le foto.'
+                    );*/
+
+                    $response['success']= false;
+                    return new Response(json_encode($response));
+                }
+
+                if($foto->getPosizione() != $key){
+                    $foto->setPosizione($key);
+                    $em->persist($foto);
+                    $em->flush();
+                }
+            }
+        }
+
+        /*$this->get('session')->getFlashBag()->add(
+            'notice',
+            'Le foto sono state riordinate, le modifiche saranno visibili solo dopo aver ricaricato la pagina.'
+        );*/
+
+        $response = array("code" => 100, "success" => true, "data"=>$data);
+        return new Response(json_encode($response));
     }
 }
