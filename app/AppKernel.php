fa186163a84b80fb7692972eb485be81d0416a0e
Mattia Clementi <mattia@qweb.eu>
2015-09-29 16:55:01 +0200

    Rimosso FMElfinderBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -8,7 +8,6 @@ class AppKernel extends Kernel
     public function registerBundles()
     {
         $bundles = array(
-            new FM\ElfinderBundle\FMElfinderBundle(),
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
