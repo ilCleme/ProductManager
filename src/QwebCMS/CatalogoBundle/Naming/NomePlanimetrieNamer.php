3829289dfd6ddc3652d0d021d019d7b777d37a9a
Mattia Clementi <mattia@qweb.eu>
2015-11-12 17:24:54 +0100

    Aggiornato Namer per planiemtrie

--- src/QwebCMS/CatalogoBundle/Naming/NomePlanimetrieNamer.php
+++ src/QwebCMS/CatalogoBundle/Naming/NomePlanimetrieNamer.php
@@ -7,7 +7,7 @@ use Oneup\UploaderBundle\Uploader\Naming\NamerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManager;
 
-class NomeNamer extends EntityManager implements NamerInterface
+class NomePlanimetrieNamer extends EntityManager implements NamerInterface
 {
     public function __construct(EntityManager $em)
     {
@@ -17,31 +17,22 @@ class NomeNamer extends EntityManager implements NamerInterface
     public function name(FileInterface $file)
     {
         $request = Request::createFromGlobals();
-        $id_album = $request->headers->get('id-album');
+        $id_product = $request->headers->get('id-product');
 
         $query = $this->em->createQuery(
             'SELECT p
             FROM QwebCMS\CatalogoBundle\Entity\TblCatalogueProduct p
-            WHERE p.idTblPhotoCat =  :id'
-        )->setParameter('id', $id_album);
+            WHERE p.idTblCatalogueProduct =  :id'
+        )->setParameter('id', $id_product);
 
         $product = $query->getResult();
-
-        $query = $this->em->createQuery(
-            'SELECT p
-            FROM QwebCMS\CatalogoBundle\Entity\TblPhoto p
-            WHERE p.idTblPhotoCat =  :id'
-        )->setParameter('id', $id_album);
-
-        $photo = $query->getResult();
 
         $productName = $this->transformSpaceToHyphen($product[0]->getTitle());
 
         $productCategories = $product[0]->getCategories();
         $productCategory = $this->transformSpaceToHyphen($productCategories[0]->getTitle());
 
-
-        return sprintf('appartamenti-%s-%s-%s.%s', strtolower($productCategory), strtolower($productName), count($photo) ,strtolower($file->getExtension()) );
+        return sprintf('appartamenti-%s-%s-%s.%s', strtolower($productCategory), strtolower($productName), "planimetria" ,strtolower($file->getExtension()) );
     }
 
     protected function transformSpaceToHyphen($string){
