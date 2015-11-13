d2a7bb5c612bbc8c3ffa4cf16fc682c87b36afe6
Mattia Clementi <mattia@qweb.eu>
2015-11-13 09:19:41 +0100

    Creato comando console per associare al nome dell' album il nome del prodotto associato

---
+++ src/QwebCMS/CatalogoBundle/Command/AlbumNameCommand.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace QwebCMS\CatalogoBundle\Command;
+
+
+use QwebCMS\CatalogoBundle\Entity\TblPhotoCat;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class AlbumNameCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('catalogo:album:nomi')
+            ->setDescription('Imposta il nome dell\'album come il nome del prodotto associato');
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $em = $this->getContainer()->get('doctrine')->getManager();
+
+        $products = $em
+            ->getRepository('QwebCMSCatalogoBundle:TblCatalogueProduct')
+            ->findAll();
+
+        foreach($products as $product){
+            $id_album = $product->getIdTblPhotoCat();
+            $album = $em
+                ->getRepository('QwebCMSCatalogoBundle:TblPhotoCat')
+                ->find($id_album);
+
+            $output->writeln("Album con id ".$id_album." collegato al prodotto con nome ".$product->getTitle()." e rinominato come tale");
+
+            $album->setNome($product->getTitle());
+            $em->persist($album);
+            $em->flush();
+        }
+    }
+}
