502296fb8704a885c88aae26f7bf67afc188ee74
Mattia Clementi <mattia@qweb.eu>
2016-05-24 17:07:57 +0200

    Aggiunta entita Allegato e sua repository

---
+++ src/QwebCMS/CatalogoBundle/Form/Type/AllegatoType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace QwebCMS\CatalogoBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class AllegatoType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('id', 'hidden')
+            ->add('idTblLingua', 'hidden')
+            ->add('nome')
+            ->add('imageFile', 'vich_file', array(
+                'required'      => false,
+                'label'         => false,
+                'allow_delete'  => false, // not mandatory, default is true
+                'download_link' => false, // not mandatory, default is true
+            ))
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => '\QwebCMS\CatalogoBundle\Entity\Allegato',
+        ));
+    }
+
+    public function getName()
+    {
+        return 'allegato';
+    }
+}
