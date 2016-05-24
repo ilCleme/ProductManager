28259f472495983b05384285b0b023241674af88
Mattia Clementi <mattia@qweb.eu>
2016-05-24 17:33:07 +0200

    Corretto nome del importer per gli allegati

--- src/QwebCMS/CatalogoBundle/Entity/Allegato.php
+++ src/QwebCMS/CatalogoBundle/Entity/Allegato.php
@@ -45,7 +45,7 @@ class Allegato
     /**
      * NOTE: This is not a mapped field of entity metadata, just a simple property.
      *
-     * @Vich\UploadableField(mapping="product_image", fileNameProperty="imageName")
+     * @Vich\UploadableField(mapping="allegati", fileNameProperty="imageName")
      *
      * @var File
      */
